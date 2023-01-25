66e1c3677293da6315f685599114cf3f1ecd43c1
Dann Johnrem A. De Guzman <dann@lapapa88.com>
2023-01-25 14:55:40 +0800

    install the tailwind and create input field

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-800 h-screen grid place-items-center">
+      <div className="bg-white w-96 p-4 rounded-md">
+        <input
+          type="text"
+          placeholder="Enter Your Location"
+          className="text-xl border-b p-1 border-gray-200 font-semibold uppercase"
+        />
+      </div>
     </div>
   );
 }
